45d8c09f15e1d03db34bb08e9137b6506bcd6692
Stephàn Costa <stephan.acosta1@outlook.com>
2024-01-16 20:32:32 -0300

    component Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const HeaderStyled = styled.div`
+
+
+`;
+
+
+export function Header() {
+    return (
+        <HeaderStyled>
+            <h1>Header</h1>
+        </HeaderStyled>
+    );
+}
