2f90f5bbef6e4b4188a6111b619457228fbf2a8b
Stephàn Costa <stephan.acosta1@outlook.com>
2024-01-16 21:13:21 -0300

    CSS Container responsividade

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,50 @@
-import { Header } from "./components/Header"
+//import { Header } from "./components/Header"
+import styled from "styled-components";
 
+
+const Container = styled.div`
+height: 100%;
+display: flex;
+flex-direction: row;
+margin: auto;
+
+
+background-color: beige;
+max-width: 800px;
+
+#left {
+  background-color: orange;
+  min-width: 180px;
+}
+
+#content {
+  background-color: yellow;
+  flex: 1;
+}
+
+#right {
+  background-color: pink;
+  min-width: 180px;
+}
+`;
 function App() {
 
   return (
     <>
-<Header />
-<div>
-  <h1>Hello World!</h1>
+  <Container>
+<div id="left">
+  <p>Left Menu</p>
 </div>
+
+<div id="content">
+  <p>Content</p>
+</div>
+
+<div id="right">
+  <p>Right Menu</p>
+</div>
+
+</Container>
     </>
   )
 }
